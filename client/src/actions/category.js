78f6ffe394f8200bb562ee0c7b163c55bad062fe
robertgeica <geicarobert@gmail.com>
2021-01-24 00:17:07 +0200

    add category/subcategory redux actions and reducers

---
+++ client/src/actions/category.js
@@ -0,0 +1,82 @@
+import axios from 'axios';
+import { CATEGORIES_LOAD, CATEGORY_LOAD, CATEGORY_LOAD_ERROR, ADD_CATEGORY, UPDATE_CATEGORY, DELETE_CATEGORY, ADD_SUBCATEGORIES } from './types';
+
+export const loadCategories = () => async (dispatch) => {
+    try {
+      const res = await axios.get('/api/category');
+  
+      dispatch({
+        type: CATEGORIES_LOAD,
+        payload: res.data,
+      });
+    } catch (error) {
+      dispatch({
+        type: CATEGORY_LOAD_ERROR,
+      });
+    }
+  };
+  
+  export const loadCategory = (id) => async (dispatch) => {
+    try {
+      const res = await axios.get("/api/category/" + id);
+  
+      dispatch({
+        type: CATEGORY_LOAD,
+        payload: res.data,
+      });
+    } catch (error) {
+      dispatch({
+        type: CATEGORY_LOAD_ERROR,
+      });
+    }
+  };
+  
+  export const handleAddCategory = (newCategory) => async (dispatch) => {
+    try {
+      const res = await axios.get("/api/category");
+  
+      const categoryObj = {
+        categoryName: newCategory.categoryName,
+        subcategories: newCategory.subcategories
+      };
+  
+      await axios.post("/api/category", categoryObj);
+      const newData = [...res.data, categoryObj];
+  
+      dispatch({
+        type: ADD_CATEGORY,
+        payload: newData,
+      });
+    } catch (error) {
+      dispatch({
+        type: CATEGORY_LOAD_ERROR,
+      });
+    }
+  };
+  
+  export const handleAddSubcategory = (id, newSubcategory) => async dispatch => {
+    try {
+      const res = await axios.get('/api/category/' + id);
+  
+      const categoryObj = {
+        ...res.data,
+        subcategories: [...res.data.subcategories, newSubcategory]
+      };
+      console.log(categoryObj);
+  
+      await axios.post('/api/category/' + id, categoryObj);
+      console.log('added label', res.data);
+  
+      const data = await axios.get('/api/category');
+      console.log(data);
+  
+      dispatch({
+        type: ADD_SUBCATEGORIES,
+        payload: data.data
+      })
+    } catch (error) {
+      dispatch({
+        type: CATEGORY_LOAD_ERROR
+      })
+    }
+  }
---
+++ client/src/reducers/category.js
@@ -0,0 +1,27 @@
+import { CATEGORIES_LOAD, CATEGORY_LOAD, CATEGORY_LOAD_ERROR, ADD_CATEGORY, UPDATE_CATEGORY, DELETE_CATEGORY, ADD_SUBCATEGORIES } from '..actions/types';
+  
+  const initialState = {
+    categories: null,
+    category: null
+  };
+  
+  export default function(state = initialState, action) {
+    const { type, payload } = action;
+  
+    switch (type) {
+      case CATEGORIES_LOAD:
+      case ADD_CATEGORY:
+      case ADD_SUBCATEGORIES:
+      case DELETE_CATEGORY:
+        return { categories: payload };
+  
+      case CATEGORY_LOAD:
+        return { ...state, category: payload };
+      
+      case CATEGORY_LOAD_ERROR:
+        return { ...state };
+  
+      default:
+        return state;
+    }
+  }
