371e0586df983f3dd6d33054e92680c909f04468
robertgeica <geicarobert@gmail.com>
2021-01-24 04:20:43 +0200

    book model

---
+++ models/Book.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  rating: [],
+  category: {
+    categoryId: {
+      type: Object,
+      required: true,
+    },
+  },
+  subcategories: [],
+  description: {
+    type: String,
+  },
+  imageUrl: {
+    type: String,
+  },
+  language: {
+    type: String,
+  },
+  pages: {
+    type: Number,
+  },
+  format: {
+    type: String,
+  },
+  fileName: {
+    type: String,
+  },
+  isbn: {
+    type: String,
+  },
+});
+
+module.exports = Book = mongoose.model("book", BookSchema);
