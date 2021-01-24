bb12a03882c25bb221d2e4959d7433cce894da04
robertgeica <geicarobert@gmail.com>
2021-01-24 03:23:50 +0200

    add category and subcategory component

---
+++ client/src/components/admin/AddCategory.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import store from "../../store/store";
+import MultiSelect from "react-multi-select-component";
+
+import {
+  handleAddCategory,
+  handleAddSubcategory,
+} from "../../actions/category";
+
+const AddCategory = ({ categories, options, selected, setSelected }) => {
+  const [categ, setCateg] = useState({});
+  const onChangeCategory = (e) => {
+    const c = {
+      categoryName: e,
+      subcategories: [],
+    };
+    setCateg(c);
+  };
+
+  const submitCategory = () => {
+    store.dispatch(handleAddCategory(categ));
+    console.log("pushed", categ);
+    setCateg({});
+  };
+
+  const [subcategory, setSubcategory] = useState();
+  const onChangeSubcategory = (e) => {
+    setSubcategory(e.target.value);
+  };
+
+  const submitSubcategory = (categName) => {
+    console.log(categName);
+    if (categName.includes(",")) {
+      console.log("Warning! Select just one category.");
+    } else {
+      const cat = categories.filter((c) => categName == c.categoryName);
+      const id = cat[0]._id;
+      store.dispatch(handleAddSubcategory(id, subcategory));
+    }
+  };
+
+  return (
+    <div className="category">
+      <div className="category-form">
+        <form className="form">
+          <input
+            className="form-field"
+            placeholder="category"
+            onChange={(e) => onChangeCategory(e.target.value)}
+          />
+          <button type="button" className="btn" onClick={submitCategory}>
+            Add category
+          </button>
+        </form>
+      </div>
+
+      <div className="subcategory-form">
+        <div>
+          <MultiSelect
+            name="category"
+            className="dropdown"
+            options={options}
+            value={selected}
+            onChange={setSelected}
+            labelledBy={"Select"}
+            hasSelectAll={false}
+            focusSearchOnOpen={true}
+          />
+        </div>
+
+        <div className="inp">
+          <input
+            className="form-field"
+            placeholder="subcategory"
+            name="subcategory"
+            onChange={(e) => onChangeSubcategory(e)}
+          />
+          <button
+            onClick={(e) =>
+              submitSubcategory(
+                e.target.parentNode.parentNode.childNodes[0].childNodes[0]
+                  .childNodes[0].childNodes[0].childNodes[0].childNodes[0]
+                  .textContent
+              )
+            }
+          >
+            Add subcategory
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddCategory;
--- client/src/components/admin/Admin.js
+++ client/src/components/admin/Admin.js
@@ -1,27 +1,56 @@
-import React from 'react';
-import { Redirect } from 'react-router-dom';
-// Redux
-import { connect } from 'react-redux';
+import React, { useState, useEffect, Fragment } from "react";
+import { Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import store from '../../store/store';
+import AddCategory from "./AddCategory";
+import { loadCategories } from "../../actions/category";
 
-const Admin = ({ auth: { isAuthenticated, loading, user } }) => {
-	if (user !== null && user.role == 'user') {
-		return <Redirect to="/" />;
-	} else {
-		return (
-			<div>
-				<p>admin page</p>
-				<p>admin page</p>
-				<p>admin page</p>
-				<p>admin page</p>
-				<p>admin page</p>
-				<p>admin page</p>
-			</div>
-		);
-	}
+const Admin = ({ auth: { isAuthenticated, loading, user }, categories }) => {
+
+	useEffect(() => {
+		store.dispatch(loadCategories());
+	}, []);
+
+	console.log(categories);
+	
+	let options = [];
+  let subcategoryOptions = [];
+  categories == null
+    ? (options = [])
+    : categories.map((c) => {
+        let obj = { label: c.categoryName, value: c.categoryName };
+        options.push(obj);
+				if(c.subcategories !== undefined) {
+					c.subcategories.map(subc => {
+						let subcategoryObj = { label: subc, value: subc };
+						subcategoryOptions = [...subcategoryOptions, subcategoryObj];
+					})
+				}
+        
+      });
+
+  const [selected, setSelected] = useState([]);
+  const [labelSelected, setLabelSelected] = useState([]);
+
+  if (user == null || user.role == "user") {
+    return <Redirect to="/" />;
+  } else {
+    return (
+      <div className="admin">
+        <AddCategory
+          categories={categories}
+          options={options}
+          selected={selected}
+          setSelected={setSelected}
+        />
+      </div>
+    );
+  }
 };
 
 const mapStateToProps = (state) => ({
-	auth: state.auth
+  auth: state.auth,
+  categories: state.category.categories,
 });
 
 export default connect(mapStateToProps)(Admin);
